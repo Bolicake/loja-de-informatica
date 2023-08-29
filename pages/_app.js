195ff628f7abdbf532022e83ac635e6f1ddba5c6
Rebeca Oliveira <131923832+Bolicake@users.noreply.github.com>
2023-08-29 02:32:58 +0000

    finishing deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/style.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+import { library } from '../utils/fontAwesome'; 
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
