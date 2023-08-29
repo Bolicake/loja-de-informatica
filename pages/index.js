2fb2215cce0ac27d32e791c2113a4f17ef19a675
Rebeca Oliveira <131923832+Bolicake@users.noreply.github.com>
2023-08-29 01:41:02 +0000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Head from "next/head"
+
+function Home() {
+    return <div>
+        <Head>
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>Loja de Informática do Obama</title>
+            <link rel="shortcut icon" href="img/MicrosoftTeams-image.png" type="image/x-icon"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,700;1,600;1,700&display=swap" rel="stylesheet"/>
+        </Head>
+    </div>
+}
